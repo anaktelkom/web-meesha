61c3d5200b6e51d49855e54201683f85e5cc5d36
anaktelkom <209965500+anaktelkom@users.noreply.github.com>
2025-05-01 21:50:08 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/(landing-page)/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import DefaultFooter from "@/components/container/footer";
+import DefaultHeader from "@/components/container/header";
+import React from "react";
+
+const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <DefaultHeader />
+      <main className="pt-[84.84px] pb-20">{children}</main>
+      <DefaultFooter />
+    </>
+  );
+};
+
+export default LandingPageLayout;
---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+import Providers from "@/providers";
+
+const interSans = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Bouquet & Florist Kebumen",
+    template: "%s | Bouquet & Florist Kebumen",
+  },
+  description: "Bouquet dan Florist Kebumen",
+  icons: {
+    icon: "#",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${interSans.variable} ${poppins.variable} antialiased font-poppins`}
+      >
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
